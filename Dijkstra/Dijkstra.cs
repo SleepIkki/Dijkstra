b2788cdab828ec05391a4e94a84212d7cc29c57f
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-25 19:35:16 +0300

    Information class to simplify Dijkstra class is added.

---
+++ Dijkstra/Dijkstra.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra
+{
+    class Dijkstra
+    {
+    }
+}
---
+++ Dijkstra/GraphPointInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dijkstra.Graph;
+
+namespace Dijkstra
+{
+    class GraphPointInfo
+    {
+        public GraphPoint Point { get; set; }
+        public bool IsUnvisited { get; set; }
+        public int EdgesWeightSum { get; set; }
+        public GraphPoint PreviousPoint { get; set; }
+
+        public GraphPointInfo(GraphPoint point)
+        {
+            Point = point;
+            IsUnvisited = true;
+            EdgesWeightSum = int.MaxValue;
+            PreviousPoint = null;
+        }
+    }
+}
