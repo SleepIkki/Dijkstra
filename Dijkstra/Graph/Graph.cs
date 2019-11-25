122e54abf9fb515999a84977af8b1e5a6578d109
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-25 19:23:48 +0300

    Graph class is ready.

---
+++ Dijkstra/Graph/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra.Graph
+{
+    class Graph
+    {
+        public List<GraphPoint> Points { get; } = new List<GraphPoint>();
+
+        public void AddPoint(int pointVal)
+        {
+            Points.Add(new GraphPoint(pointVal));
+        }
+
+        public GraphPoint FindPoint(int pointValue)
+        {
+            foreach (var p in Points)
+            {
+                if (p.Value.Equals(pointValue))
+                {
+                    return p;
+                }
+            }
+
+            return null;
+        }
+
+        public void AddEdge(int firstVal, int secondVal, int weight)
+        {
+            var p1 = FindPoint(firstVal);
+            var p2 = FindPoint(secondVal);
+            if (p2 != null && p1 != null)
+            {
+                p1.AddEdge(p2, weight);
+                p2.AddEdge(p1, weight);
+            }
+        }
+    }
+}
