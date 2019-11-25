6243eaca2ecd5464b4e5104ea3bf7886eec1db37
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-25 19:12:57 +0300

    GraphPoint and GraphEdge are created.

---
+++ Dijkstra/Graph/GraphEdge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra.Graph
+{
+    class GraphEdge
+    {
+        public GraphPoint ConnectedPoint { get; }
+        public int EdgeWeight { get; }
+        public GraphEdge(GraphPoint connectedPoint, int weight)
+        {
+            ConnectedPoint = connectedPoint;
+            EdgeWeight = weight;
+        }
+    }
+}
---
+++ Dijkstra/Graph/GraphPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra.Graph
+{
+    class GraphPoint
+    {
+        public int Value { get; }
+        public List<GraphEdge> Edges { get; }
+
+        public GraphPoint(int value)
+        {
+            Value = value;
+            Edges = new List<GraphEdge>();
+        }
+
+        public void AddEdge(GraphEdge edge) => Edges.Add(edge);
+
+        public void AddEdge(GraphPoint point, int edgeWeight)
+        {
+            AddEdge(new GraphEdge(point, edgeWeight));
+        }
+
+        public override string ToString() => Value.ToString();
+    }
+}
